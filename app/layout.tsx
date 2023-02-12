3e63acbac026c88785c14f9f34ba9b4304e0d960
nikkoabucejo <me@nikkoabucejo.com>
2023-02-12 21:33:02 +0800

    chore: add root layout

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import "@/core/styles/globals.css";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: Props) => {
+  return (
+    <html>
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default Layout;
